9731ba212f9b46ec2bf42e3536f1f0471278036b
Dipanshu <dipanshubisen15@gmail.com>
2026-06-03 16:29:19 +0530

    task

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,1 +1,16 @@
-hi
+
+# AWS Provider Name
+terraform {
+  required_providers {
+    aws = {
+         source  = "hashicorp/aws"
+         version = "6.40.0"
+    }
+  }
+}
+
+# AWS Default Regions
+
+Provider "aws" {
+   regions = "ap-south-1"
+}
