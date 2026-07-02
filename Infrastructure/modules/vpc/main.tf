aaeae54326850d4beb69693473d80220f8776614
Dipanshu <dipanshubisen15@gmail.com>
2026-07-02 21:26:07 +0530

    change in vpc

--- Infrastructure/modules/vpc/main.tf
+++ Infrastructure/modules/vpc/main.tf
@@ -48,3 +48,71 @@ resource "aws_subnet" "private" {
     Environment = var.environment
   }
 }
+
+#Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  count = length(var.public_subnets)
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.environment}-nat-eip-${count.index + 1}"
+    Environment = var.environment
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "main" {
+  count = length(var.public_subnets)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id = aws_subnet.public[count.index].id
+
+  tags = {
+    Name = "${var.environment}-nat-${count.index + 1}"
+    Environment = var.environment
+  }
+}
+
+#Public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+    Environment = var.environment
+  }
+}
+
+# Private route table
+resource "aws_route_table" "private" {
+  count = length(var.private_subnets)
+  vpc_id = aws_vpc.main.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway[count.index].id
+  }
+
+  tags = {
+    Name = "${var.environment}-private-rt-${count.index + 1}"
+    Environment = var.environment
+  }
+}
+
+
+# Route table association
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets)
+  subnet_id = aws_subnet.private[count.index]
+  route_table_id = aws_route_table.private.id
+}
