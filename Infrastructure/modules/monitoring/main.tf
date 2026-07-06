ab4cb37ef4dd2e0200ed0b126d23b7b1c45f20c5
Dipanshu <dipanshubisen15@gmail.com>
2026-07-06 15:10:28 +0530

    creade monitoring

--- Infrastructure/modules/monitoring/main.tf
+++ Infrastructure/modules/monitoring/main.tf
@@ -10,3 +10,48 @@ resource "aws_cloudwatch_log_group" "application" {
     Environment = var.environment
   }
 }
+
+#RDS cloudwatch alarm
+resource "aws_cloudwatch_metric_alarm" "rds_cpu" {
+  alarm_name = "${var.environment}-rds-high-cpu"
+  comparison_operator = "GreaterThanThershold"
+  evaluation_periods = "2"
+  metric_name = "CPUUtilization"
+  namespace = "AWS/RDS"
+  period = "300"
+  statistic = "Average"
+  threshold = "80"
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  alarm_actions = []
+
+  dimensions = {
+    DBInstanceIdentifie = var.rds_instance_id
+  }
+
+  tags = {
+    Name        = "${var.environment}-rds-cpu-alarm"
+    Environment = var.environment
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "reds_memory" {
+  alarm_name = "${var.environment}-rds-low-memory"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods = "2"
+  metric_name = "FreeableMemory"
+  namespace = "AWS/RDS"
+  period = "300"
+  statistic = "Average"
+  threshold = "268435456" # 256MB in bytes
+  alarm_description = "This metric monitors RDS free memory"
+  alarm_actions = []
+   
+  dimensions = {
+    DBInstanceIdentifier = var.rds_instance_id
+  }
+
+  tags = {
+    Name        = "${var.environment}-rds-memory-alarm"
+    Environment = var.environment
+  }
+}
--- Infrastructure/modules/monitoring/variables.tf
+++ Infrastructure/modules/monitoring/variables.tf
@@ -2,3 +2,8 @@ variable "environment" {
   description = "Environment name"
   type = string
 }
+
+variable "rds_instance_id" {
+  description = "RDS insatnace ID"
+  type = string
+}
