1bad49afe500c3c1cf16c7c3d8fe143f369675b9
Dipanshu <dipanshubisen15@gmail.com>
2026-07-06 16:59:53 +0530

    add output

--- Infrastructure/modules/security/outputs.tf
+++ Infrastructure/modules/security/outputs.tf
@@ -2,3 +2,18 @@ output "db_security_group_id" {
   description = "DB security group ID"
   value = aws_security_group.db.id
 }
+
+output "alb_security_group_id" {
+  description = "ALB security group Id"
+  value = aws_security_group.alb.id
+}
+
+output "app_security_group_id" {
+  description = "APP security group ID"
+  value = aws_security_group.app.id
+}
+
+output "bastion_security_group_id" {
+  description = "Bastion security group ID"
+  value = aws_security_group.bastion.id
+}
