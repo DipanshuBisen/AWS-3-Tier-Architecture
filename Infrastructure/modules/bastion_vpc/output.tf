7da2253b9b6fed53e36fb6de5e6dac28fcf94e89
Dipanshu <dipanshubisen15@gmail.com>
2026-07-13 19:17:40 +0530

    resolved

--- Infrastructure/modules/bastion_vpc/output.tf
+++ Infrastructure/modules/bastion_vpc/output.tf
@@ -6,12 +6,9 @@ output "public_subnet_id" {
 output "bastion_vpc_id" {
   description = "Bastion VPC ID"
   value = aws_vpc.bastion.id
-<<<<<<< HEAD
-=======
 }
 
 output "bastion_public_route_table_id" {
   description = "Route table ID of bastion public"
   value = aws_route_table.bastion.id
->>>>>>> 354416a (start)
 }
